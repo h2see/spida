0cfa7d5e6048caaab740468843aedb65acc3543d
Holden Craig <holden.h.craig@gmail.com>
2023-05-26 17:05:48 -0400

    update setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="spida",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
